80b023042f610c94101151f7f71ad6ddf1717abf
paultegelaar@gmail.com <paultegelaar@gmail.com@15ec070d-3c3f-a127-75af-7c32b3fc42af>
2015-02-23 10:14:32 +0000

    Several small fixes

--- src/main/java/com/pte/liquid/relay/marshaller/json/JsonMarshaller.java
+++ src/main/java/com/pte/liquid/relay/marshaller/json/JsonMarshaller.java
@@ -1,5 +1,7 @@
 package com.pte.liquid.relay.marshaller.json;
 
+import java.text.DateFormat;
+
 import org.apache.log4j.Logger;
 
 import com.google.gson.Gson;
@@ -16,18 +18,22 @@ public class JsonMarshaller implements Marshaller{
 	public JsonMarshaller(){
 		GsonBuilder gsonBuilder = new GsonBuilder();
 		gsonBuilder.excludeFieldsWithoutExposeAnnotation();
-		gson = gsonBuilder.create();		
+		gsonBuilder.setDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSZ");
+		gson = gsonBuilder.create();
+		
 	}
 	
 	@Override
 	public synchronized String marshal(Message message) throws RelayException {
-		logger.debug("Marshalling json message...");		
+		logger.debug("Marshalling json message...");
+		logger.info("1: " + gson.toJson(message));
 		return gson.toJson(message);
 	}
 
 	@Override
 	public synchronized Message unmarshal(String message) throws RelayException {
 		logger.debug("Unarshalling json message...");
+		logger.info("2: " + message);
 		return gson.fromJson(message, Message.class);
 	}
 	
